70f72de3bd0267098d1a8612d4ad39791250111a
Ananyaa Shrivastava <45946392+Ananyaas@users.noreply.github.com>
2022-01-05 22:56:11 +0530

    Time: 11 ms (5.63%), Space: 6 MB (33.22%) - LeetHub

---
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int integerReplacement(int n) {
+        if(n==INT_MAX)
+           return 32;
+        if(n==1)
+            return 0;
+     
+        if(n%2==0)
+            return 1+integerReplacement(n/2);
+        else{
+            if((n-1 & n-2) ==0)    
+            return 1+integerReplacement(n-1);
+            if((n+1 & n)==0)    
+            return 1+integerReplacement(n+1);
+            else
+             return 1+min(integerReplacement(n+1),integerReplacement(n-1));   
+        }
+        return 0;
+        
+    }
+};
