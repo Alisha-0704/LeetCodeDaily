dd6e0235415625a68bed4009717700c6c987657c
Ananyaa Shrivastava <45946392+Ananyaas@users.noreply.github.com>
2022-01-06 13:45:12 +0530

    Time: 58 ms (5.10%), Space: 12.8 MB (6.18%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-        static bool comp(vector<int> lhs, vector<int> rhs)
-    {
-        if(lhs[1]<rhs[1] || (lhs[1]==rhs[1] && lhs[2]<rhs[2]))
-            return true ;
-        return false ;
-    }
-        
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         if(trips.size()==0)
             return false;
         
-       
-        sort(trips.begin(),trips.end(),comp);
-       int count=0; 
-priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; 
         
-       
-        for(int i=0;i<trips.size();i++)
-        {
-		
-            while(!pq.empty() && pq.top().first<=trips[i][1])
-            {
-                count-=pq.top().second ;
-                pq.pop() ;
+        
+        vector<int>count(10001,0);
+        int maxdrop=0;
+        for(int i=0;i<trips.size();i++){
+            for(int j=trips[i][1] ;j<trips[i][2];j++){
+                count[j]+=trips[i][0];
+                
             }
-            
-			
-            pq.push({trips[i][2],trips[i][0]}) ;
-            count+=trips[i][0] ;
-			
-		
-            if(count>capacity)
-                return false ;
+            maxdrop=max(maxdrop,trips[i][2]);
         }
+        int maxim=0;
+        for(int i=0;i<=maxdrop;i++){
+            maxim=max(maxim,count[i]);
+        }
+        if(maxim>capacity)
+            return false;
         return true;
-        
     }
 };
