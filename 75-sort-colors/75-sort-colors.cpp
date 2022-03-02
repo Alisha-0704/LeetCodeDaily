fead2ed9d9e6c21d8d8806d092742f683e1ded8b
Ananyaa Shrivastava <45946392+Ananyaas@users.noreply.github.com>
2022-03-02 16:38:38 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.56%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        map<int,int>m;
+        
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        
+        for(int i=0;i<m[0];i++)
+            nums[i]=0;
+        for(int i=m[0];i<m[1]+m[0];i++)
+            nums[i]=1;
+        for(int i=m[1]+m[0];i<nums.size();i++)
+            nums[i]=2;
+        
+        return;
+    }
+};
