114022806246ec4989d22a7517c4a2020d54d33a
Ananyaa Shrivastava <45946392+Ananyaas@users.noreply.github.com>
2022-03-05 17:41:56 +0530

    Time: 8 ms (8.93%), Space: 5.8 MB (88.84%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,11 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
             return false;
-        while(n>0){
-            if(n%2==1 && n!=1)
-                return false;
-            n=n/2;
-        }
+        if(n&(n-1))
+            return false;
         return true;
         
     }
